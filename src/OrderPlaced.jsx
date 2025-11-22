309e356524992a96222983f14e34eb7ce1fb843d
anushkakeni <kenianushka@gmail.com>
2025-11-22 20:09:14 +0530

    e-commerce

--- src/OrderPlaced.jsx
+++ src/OrderPlaced.jsx
@@ -1,7 +1,24 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
 
 export default function OrderPlaced() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/");
+    }, 6000);
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+
   return (
-    <div>OrderPlaced</div>
+    <div className="success-container">
+      <div className="success-box">
+        <div className="tick-mark">✔</div>
+        <h2>Order Placed Successfully!</h2>
+        <p>Thank you for shopping with us.</p>
+      </div>
+    </div>
   )
 }
